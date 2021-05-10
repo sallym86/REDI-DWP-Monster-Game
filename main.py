8418e69350f2dc377ae226f4836e041caeebc446
Diana-Matar <Diana-Matar@users.noreply.github.com>
2021-05-10 13:34:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+player = {'name': 'Lili', 'attack': 10, 'heal': 16, 'health': 100}
+monster = {'name': 'Max', 'attack': 12, 'health': 100}
+
+print('Welcome to Monster Game!!')
+print('Please select action')
+print('1) Attack')
+print('2) Heal')
+
+player_choice = input()
+
+if player_choice == '1':
+  print('Attack')
+  
+elif player_choice == '2':
+  print('Heal player')
+
+else:
+  print('Invalid Input')  
