df9b16000491d2e8432087612eeb96cb3a6e3a8d
Titijohn <84205987+Titijohn@users.noreply.github.com>
2021-05-15 11:02:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ player = {'name': 'Lili', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name': 'Max', 'attack': 12, 'health': 100}
 
 print('Welcome to Monster Game!!')
+print('Welcome to 21sw_Coding class Monster Game!!')
 print('Please select action')
 print('1) Attack')
 print('2) Heal')
@@ -15,4 +16,5 @@ elif player_choice == '2':
   print('Heal player')
 
 else:
-  print('Something went wrong', 'Probably becasue Lucía edited it')  
+  print('Something went wrong', 'Probably becasue Lucía edited it') 
+  
